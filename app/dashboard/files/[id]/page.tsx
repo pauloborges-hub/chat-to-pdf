4b21b33462ddfbd8d0dd3f215497df7bb72773f5
Paulo Borges <victorborges296@hotmail.com>
2025-02-08 17:21:52 -0300

    PDFView component added

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,6 +1,33 @@
-function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
+import PDFView from "@/components/PDFView"
+import { adminDb } from "@/firebaseAdmin"
+import { auth } from "@clerk/nextjs/server"
+
+async function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
+   auth.protect()
+   const { userId } = await auth()
+
+   const ref = await adminDb
+      .collection("users")
+      .doc(userId!)
+      .collection("files")
+      .doc(id)
+      .get()
+   
+      const url = ref.data()?.downloadUrl
+
    return (
-      <div>{id}</div>
+      <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+         {/* Right */}
+         <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+            {/* Chat */}
+         </div>
+
+         {/* Left */}
+         <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+            {/* PDFView */}
+            <PDFView url={url} />
+         </div>
+      </div>
    )
 }
 
