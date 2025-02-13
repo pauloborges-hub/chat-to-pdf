47d016f28523881344df9afa71bcf7cae8d8a057
Paulo Borges <victorborges296@hotmail.com>
2025-02-13 09:15:58 -0300

    Stripe Portal created for management of the subscription and payment for the user

---
+++ actions/createStripePortal.ts
@@ -0,0 +1,36 @@
+"use server"
+
+import { adminDb } from "@/firebaseAdmin";
+import getBaseUrl from "@/lib/getBaseUrl";
+import stripe from "@/lib/stripe";
+import { auth } from "@clerk/nextjs/server";
+import Stripe from "stripe";
+
+export async function createStripePortal() {
+   auth.protect()
+
+   const { userId } = await auth()
+
+   if (!userId) {
+      throw new Error("User not found")
+   }
+
+   // Get the customer ID from firebase
+   const user = await adminDb
+      .collection("users")
+      .doc(userId)
+      .get()
+
+   const stripeCustomerId = user.data()?.stripeCustomerId
+
+   if (!stripeCustomerId) {
+      throw new Error("Stripe customer not found")
+   }
+
+   const sessions = await stripe.billingPortal.sessions.create({
+      customer: stripeCustomerId,
+      return_url: `${getBaseUrl()}/dashboard`
+   })
+
+   return sessions.url
+}
